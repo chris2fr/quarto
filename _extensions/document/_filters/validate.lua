d0074df5a1cdee00ddeb602934f876a697513164
Chris Mann <chris@lesgtrandsvoisins.com>
2026-08-26 10:18:09 +0200

    Document has a more elaborate validation of metadata than lettre

--- _extensions/document/_filters/validate.lua
+++ _extensions/document/_filters/validate.lua
@@ -29,7 +29,7 @@ local upstream_meta = {
   -- Quarto/Pandoc reserved or auto-injected
   'authors', 'by-author', 'language', 'toc-title', 'quarto-version',
   'include-before', 'include-after', 'fig-responsive', 'revealjs-plugins',
-  'colorlinks', 'linkcolor', 'block-headings', 'biblio-config', 'document-css',
+  'colorlinks', 'linkcolor', 'block-headings', 'biblio-config', 'document-css', 'format', 'metadata-files'
 }
 
 function Pandoc(doc)
